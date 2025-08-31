f717ced5ceb93e856595b9c98b2853b74b90f7fa
WorldCheng <1913711216@qq.com>
2025-08-31 20:37:48 +0800

    add another packge

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include "process.h"
+#include <Eigen/Dense>
 using std::cout, std::endl;
+
 
 void Process::planProcess(){
   cout << "this is process" <<endl;
+
+  Eigen::MatrixXd m(2,2);
+  m(0, 0) = 3;
+  m(1, 0) = 5;
+  m(0, 1) = -1;
+  m(1, 1) = m(1, 0) + m(0, 1);
+  cout << m <<endl;
 }
